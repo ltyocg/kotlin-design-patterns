ff0571660b6047645ef33c505456b8ae2ecc74de
John Liu <ltyocg@163.com>
2021-07-11 23:17:16 +0800

    Convert to Kotlin style.

--- active-object/src/main/kotlin/com/ltyocg/activeobject/main.kt
+++ active-object/src/main/kotlin/com/ltyocg/activeobject/main.kt
@@ -5,9 +5,9 @@ private const val NUM_CREATURES = 3
 fun main() {
     val creatures = (0 until NUM_CREATURES)
         .map {
-            Orc(Orc::class.simpleName + it).apply {
-                eat()
-                roam()
+            Orc(Orc::class.simpleName + it).also { orc ->
+                orc.eat()
+                orc.roam()
             }
         }
         .toList()
