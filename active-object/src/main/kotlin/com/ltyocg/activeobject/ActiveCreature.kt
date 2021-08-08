5db0b79bd38c386e553ed404319f2b4f95934622
John Liu <ltyocg@163.com>
2021-08-08 15:17:59 +0800

    Convert to Kotlin style.

--- active-object/src/main/kotlin/com/ltyocg/activeobject/ActiveCreature.kt
+++ active-object/src/main/kotlin/com/ltyocg/activeobject/ActiveCreature.kt
@@ -1,45 +1,36 @@
 package com.ltyocg.activeobject
 
+import kotlinx.coroutines.*
 import org.slf4j.LoggerFactory
 import java.util.concurrent.Executors
-import java.util.concurrent.RejectedExecutionException
 
 abstract class ActiveCreature
 internal constructor(val name: String) {
     private val log = LoggerFactory.getLogger(this::class.java)
-    private val executorService = Executors.newSingleThreadExecutor()
+    private val newSingleThreadExecutor = Executors.newSingleThreadExecutor()
+    private val coroutineScope = CoroutineScope(newSingleThreadExecutor.asCoroutineDispatcher())
     var status = 0
         private set
+    private val onCompletionHandler: CompletionHandler = {
+        if (it != null && status != 0) log.error("Thread was interrupted. --> {}", it.localizedMessage)
+    }
 
     fun eat() {
-        submit {
+        coroutineScope.launch {
             log.info("{} is eating!", name)
             log.info("{} has finished eating!", name)
-        }
+        }.invokeOnCompletion(onCompletionHandler)
     }
 
     fun roam() {
-        submit {
+        coroutineScope.launch {
             log.info("{} has started to roam in the wastelands.", name)
-        }
+        }.invokeOnCompletion(onCompletionHandler)
     }
 
     fun kill(status: Int = 0) {
         this.status = status
-        executorService.shutdownNow()
-    }
-
-    private fun submit(task: Runnable) {
-        try {
-            executorService.submit {
-                try {
-                    task.run()
-                } catch (e: InterruptedException) {
-                    if (status != 0) log.error("Thread was interrupted. --> {}", e.localizedMessage)
-                    executorService.shutdown()
-                }
-            }
-        } catch (e: RejectedExecutionException) {
-        }
+        coroutineScope.cancel()
+        newSingleThreadExecutor.shutdownNow()
     }
 }
--- active-object/src/main/kotlin/com/ltyocg/activeobject/main.kt
+++ active-object/src/main/kotlin/com/ltyocg/activeobject/main.kt
@@ -4,11 +4,10 @@ private const val NUM_CREATURES = 3
 
 fun main() {
     val creatures = (0 until NUM_CREATURES)
-        .map {
-            Orc(Orc::class.simpleName + it).also { orc ->
-                orc.eat()
-                orc.roam()
-            }
+        .map { Orc(Orc::class.simpleName + it) }
+        .onEach {
+            it.eat()
+            it.roam()
         }
         .toList()
     Thread.sleep(1000)
